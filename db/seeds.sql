ef3b0935b1e1fe0e1f1c321e912bcc273ddce4c2
Joseph Morgan <jmorgan9@yahoo.com>
2017-09-23 13:51:32 -0700

    First Commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES 
+	("Cheeseburger", true, ""),
+	("Veggie Burger", false, "");
+	("Elk Burger", false, "");
+	("Whooper", false, "");
+	("Big Boy Burger", false, "");
+
